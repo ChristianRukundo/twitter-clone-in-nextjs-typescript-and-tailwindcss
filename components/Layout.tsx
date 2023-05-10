2e50016e08b3b1f37074c1beb970c3aaba4e72af
ChristianMrCoder <siboruremarukundoc@gmail.com>
2023-05-10 08:38:33 +0200

    commit

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl">{children}</div>
+    </div>
+  );
+};
+
+
+export default Layout;
