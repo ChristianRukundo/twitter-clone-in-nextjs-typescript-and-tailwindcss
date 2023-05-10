ee5c1ec7c300ee1462a64272c4c896b45acfae80
ChristianMrCoder <siboruremarukundoc@gmail.com>
2023-05-10 08:38:43 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
